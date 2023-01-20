32d4dfd51eaa831234f162d6f32d8f9d31637e1f
Moralex <oscar.morales@cumplo.com>
2023-01-20 15:41:51 -0600

    refactor in costController, apply bestpractice

--- app/controllers/costController.js
+++ app/controllers/costController.js
@@ -3,73 +3,33 @@ const axios = require('axios');
 const Record = require('../models/record');
 const Price = require('../models/price');
 require('dotenv').config();
-const APIKEY = process.env.APIKEY; //api key google maps
-const APPID = process.env.APPID; //API key from weather
-const MAPS_URL = process.env.MAPS_URL + APIKEY;
-const WEATHER_ZIP_CODE_URL = process.env.WEATHER_ZIP_CODE_URL + APPID;
+//APIKEY google maps, APPID key from weather
+const {APIKEY, APPID, WEATHER_ZIP_CODE_URL, MAPS_URL} = process.env;
+const MAP_URL = MAPS_URL + APIKEY;
+const WEATHER_URL = WEATHER_ZIP_CODE_URL + APPID;
 const controllerName = 'CostController';
-
-const error = console.error;
 
 async function getCost(req, res) {
 	const {latorigen, lonorigen, latdestino, londestino} = req.params;
 	const PARAMS = `&origins=${latorigen},${lonorigen}&destinations=${latdestino},${londestino}`;
-	info(`URL ${MAPS_URL}${PARAMS}`);
 	const ORIGINPARAMS = `&lat=${latorigen}&lon=${lonorigen}`;
 	const DESTINATIONPARAMS = `&lat=${latdestino}&lon=${londestino}`;
-	let resultData = [];
 
-	if (validateParams(latorigen, lonorigen, latdestino, londestino)) {
-		const distance = await getDistanceByCoordinates(MAPS_URL + PARAMS);
-		const {destination_addresses, origin_addresses} = distance;
-		resultData.push(distance);
+	if (!validateParams(req.params)) sendError(res, 'Latitud y longitud deben de ser numericos');
+	const route = await getDistanceByCoordinates(MAP_URL + PARAMS);
+	const {destination_addresses, origin_addresses, rows} = route;
+	const [elements] = rows[0].elements;
+	if (elements.status === 'ZERO_RESULTS' || !destination_addresses || !origin_addresses)
+		sendError(res, elements, 'there are not records');
 
-		// const {destination_addresses, origin_addresses} = response.data;
-		if (destination_addresses && origin_addresses) {
-			const originWeather = await getWeatherByCoordinates(
-				WEATHER_ZIP_CODE_URL + ORIGINPARAMS
-			);
-			const destinationWeather = await getWeatherByCoordinates(
-				WEATHER_ZIP_CODE_URL + DESTINATIONPARAMS
-			);
-			resultData.push(originWeather);
-			resultData.push(destinationWeather);
+	const originWeather = await getWeatherByCoordinates(WEATHER_URL + ORIGINPARAMS);
+	const destinationWeather = await getWeatherByCoordinates(WEATHER_URL + DESTINATIONPARAMS);
 
-			const origin = {
-				place: resultData[0].origin_addresses[0],
-				name: resultData[1].name,
-				description: resultData[1].weather[0].description,
-				temp: resultData[1].main.temp,
-				icon:
-					'http://openweathermap.org/img/wn/' + resultData[1].weather[0].icon + '@2x.png'
-			};
-			const destination = {
-				place: resultData[0].destination_addresses[0],
-				name: resultData[2].name,
-				description: resultData[2].weather[0].description,
-				temp: resultData[2].main.temp,
-				icon:
-					'http://openweathermap.org/img/wn/' + resultData[2].weather[0].icon + '@2x.png'
-			};
-			let distance = 0;
-			let time = 0;
-			if (resultData[0].rows[0].elements[0].status != 'ZERO_RESULTS') {
-				distance = resultData[0].rows[0].elements[0].distance.text;
-				time = resultData[0].rows[0].elements[0].duration.text;
-			} else {
-				log('No hay resultados para la distancia| ', resultData[0].rows[0].elements[0]);
-			}
+	const origin = getPlaceDetails(origin_addresses, originWeather);
+	const destination = getPlaceDetails(destination_addresses, destinationWeather);
 
-			const precio = await Price.findOne({}).clone();
-			log(`precio | ${precio} ${resultData} ${origin} ${destination} ${distance} ${time}`);
-			await buildResponse(precio, resultData, origin, destination, distance, time, res);
-		} else {
-			console.log('response.data: vacio ', response.data);
-			sendError(res, {params: PARAMS, status: 'Location not found'});
-		}
-	} else {
-		sendError(res, 'Latitud y longitud deben de ser numericos');
-	}
+	const cost = await calculateCost(elements, originWeather, destinationWeather);
+	await buildResponse(res, cost, origin, destination, elements);
 }
 async function getWeatherByCoordinates(url) {
 	log(' URL : ', url);
@@ -86,6 +46,7 @@ async function getWeatherByCoordinates(url) {
 }
 
 async function getDistanceByCoordinates(url) {
+	log(' URL : ', url);
 	return await axios
 		.get(url)
 		.then(function (response) {
@@ -98,27 +59,19 @@ async function getDistanceByCoordinates(url) {
 		});
 }
 
-async function buildResponse(prices, resultData, origin, destination, distance, time, res) {
-	let input =
-		resultData[0].rows[0].elements[0].status != 'ZERO_RESULTS'
-			? calculateCost(
-					prices,
-					resultData[0].rows[0].elements[0].distance.value,
-					resultData[0].rows[0].elements[0].duration.value,
-					resultData[1].weather[0].id,
-					resultData[2].weather[0].id
-			  )
-			: 0;
-	let cost = isNaN(input.costo) ? 0 : input.costo.toFixed(2);
+async function buildResponse(res, cost, origin, destination, elements) {
+	const distance = elements.distance.text;
+	const time = elements.duration.text;
+	let input = cost;
 	let output = {
 		origin,
 		destination,
-		cost,
+		cost: cost.costo,
 		distance,
 		time,
-		message: '#AmoTirarCodigo&LosDesplieguesContinuos'
+		message: '#AmoTirarCodigo'
 	};
-	let recordSaved = await saveRecord(input, output);
+	await saveRecord(input, output);
 	dataValidation(res, output, controllerName);
 }
 
@@ -133,26 +86,34 @@ async function saveRecord(input, output) {
 	});
 }
 
-function validateParams(lat, lon, lat2, lon2) {
-	log(lat, lon, lat2, lon2);
-	if (isValidFloat(lat) && isValidFloat(lon) && isValidFloat(lat2) && isValidFloat(lon2)) {
+function validateParams(params) {
+	const {latorigen, lonorigen, latdestino, londestino} = params;
+	log(latorigen, lonorigen, latdestino, londestino);
+	if (
+		isValidFloat(latorigen) &&
+		isValidFloat(lonorigen) &&
+		isValidFloat(latdestino) &&
+		isValidFloat(londestino)
+	) {
 		return true;
 	}
 	return false;
 }
-
-function calculateCost(input, km, time, weatherCodeOrigin, weatherCodeDestination) {
-	/**Se agrega modificación , dado que inicialmente se va contratar un servicio de transporte que recoja las muestras */
-	console.log('getWeatherFactor |', getWeatherFactor(input.factorclima, weatherCodeOrigin));
-	const banderazo = parseFloat(input.gasolina),
-		kilometrosXRecorrer = km / 1000, //numero de kilometros a recorrer
+// TODO: create a new formula
+async function calculateCost(elements, originWeather, destinationWeather) {
+	const params = await Price.findOne({}).clone();
+	const {gasolina, factorclima, factortiempo, costoChoferXMin} = params;
+	const {distance, duration} = elements;
+	elements;
+	const banderazo = parseFloat(gasolina),
+		kilometrosXRecorrer = distance.value / 1000, //kilometros a recorrer
 		factorDeClima =
-			(getWeatherFactor(input.factorclima, weatherCodeOrigin) +
-				getWeatherFactor(input.factorclima, weatherCodeDestination)) /
+			(getWeatherFactor(factorclima, originWeather) +
+				getWeatherFactor(factorclima, destinationWeather)) /
 			2, // cual es el factor del clima
-		factorTiempo = getDayTime(input.factortiempo), // Dia, tarde o Noche
-		tiempoARecorrerMin = time / 60, // tiempo que se tardará en recorrer esa distancia (minutos)
-		tarifaXKilometro = input.costoChoferXMin; //costo del chofer por minuto(en pesos)
+		factorTiempo = getDayTime(factortiempo), // Dia, tarde o Noche
+		tiempoARecorrerMin = duration.value / 60, // tiempo que se tardará en recorrer esa distancia (minutos)
+		tarifaXKilometro = costoChoferXMin; //costo del chofer por minuto(en pesos)
 	//valor = [(gastosXkilometroGasolina * NumeroKilometrosARecorrer) (factorDeClima)] + [FactorTiempo * tiempoARecorrerMin * costoChoferXMin]
 	//valor = [(1.8 * 6)(1.6)] + (0.5*10) = (10.8 * 1.6) + 5 = 6.48  +5 = 11.48
 	return {
@@ -161,14 +122,18 @@ function calculateCost(input, km, time, weatherCodeOrigin, weatherCodeDestinatio
 		factorDeClima,
 		factorTiempo,
 		tiempoARecorrerMin,
-		banderazo,
-		kilometrosXRecorrer,
-		factorDeClima,
-		tiempoARecorrerMin,
-		costo: banderazo + tarifaXKilometro * kilometrosXRecorrer
+		costo: (banderazo + tarifaXKilometro * kilometrosXRecorrer).toFixed(2)
 	};
 }
-
+function getPlaceDetails(addresses, weather) {
+	return {
+		place: addresses[0],
+		name: weather.name,
+		description: weather.weather[0].description,
+		temp: weather.main.temp,
+		icon: 'http://openweathermap.org/img/wn/' + weather.weather[0].icon + '@2x.png'
+	};
+}
 function getWeatherFactor(weatherPrice, weather) {
 	const condtions = weatherPrice.find(w => w.code == weather);
 	return condtions ? condtions.value : 1.5;
